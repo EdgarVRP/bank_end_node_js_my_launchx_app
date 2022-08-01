1ce74654c549d4e1ed66909efe4f0ef1d30f170d
EdgarVRP <edgarvrp22@gmail.com>
2022-08-01 13:15:51 -0500

    Prueba Realizada correctamente

--- ​missionCommander.test.js
+++ ​missionCommander.test.js
@@ -1,4 +1,4 @@
-const MissionCommander = require('./../app/missionCommander');
+const MissionCommander = require('./app/missionCommander');
 describe("Unit Tests for Mission Commander Class", () => {
   test('1) Create a mission commander objet', () => {
     const myMissionCommander = new MissionCommander("Woopa")
